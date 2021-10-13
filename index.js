b1d9d802623e9f7e267ad7a4629390802d2ad928
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-10-12 21:42:24 -0400

    started the buildHTML function and cleaned up some of the class files

--- index.js
+++ index.js
@@ -145,12 +145,70 @@ function init() {
           askInt();
           break;
         default:
+          buildHTML()
           break;
       }
     };
   ask();
 }
+const buildHTML = () => {
+  fs.writeFile(
+    "./src/index.html",
+    `<!DOCTYPE html>
+<html lang='en-us'>
+  <head>
+    <meta charset='UTF-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+    <link
+      rel='stylesheet'
+      href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css'
+      integrity='sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU'
+      crossorigin='anonymous'
+    />
+    <link rel='stylesheet' href='./assets/css/reset.css' />
+    <link rel='stylesheet' href='./assets/css/style.css' />
+    <title>My Team</title>
+  </head>
+  <body>
+    <header class='container-fluid text-center'>
+      <h1 class='text-center h1'>My Team</h1>
+    </header>
+    <main class='container'>
+      <div class='row'>
+        <div class='col'>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">${id}</li>
+              <li class="list-group-item">${email}</li>
+              <li class="list-group-item">${wildcard}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer></footer>
+    <script
+      src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js'
+      integrity='sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/'
+      crossorigin='anonymous'
+    ></script>
+    <script
+      src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js'
+      integrity='sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ'
+      crossorigin='anonymous'
+    ></script>
+  </body>
+</html>
 
+  `,
+    "utf8"
+  );
+}
 // inquirer.prompt(questions).then((answers) => {
 //   fs.appendFile(`./dist/index.html`, generateHTML(answers), (err) =>
 //     err
